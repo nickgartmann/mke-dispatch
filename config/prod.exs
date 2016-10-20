823616b9b64a435af8867652f9e82bc90a20c028
Nick Gartmann <nick.gartmann@gmail.com>
2016-10-20 15:04:35 -0500

    try putting the url in

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :mke_police, MkePolice.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80]
+  url: [host: "cryptic-beyond-26319.herokuapp.com"]
 
 config :mke_police, MkePolice.Repo,
   adapter: Ecto.Adapters.Postgres,
